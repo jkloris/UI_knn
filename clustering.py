3284f16abb320612f93cc8221f6ed2ef31a6a8cf
jkloris <kapusta.jergus@gmail.com>
2021-11-30 14:24:26 +0100

    vyhladavanie najkratsej cesty cez kd stromy

--- clustering.py
+++ clustering.py
@@ -26,31 +26,32 @@ class KDTree:
         self.trainingSet = self.trainingSet[self.trainingSet[:,0].argsort()]
         # print(trainingSet)
         med = int(len(self.trainingSet) / 2)
-        leftSet = self.trainingSet[:med-1]
-        rightSet = self.trainingSet[med:]
+        leftSet = self.trainingSet[:med]
+        rightSet = self.trainingSet[med+1:]
 
         root = Node(self.trainingSet[med])
 
-        # rightSet by mal byt vacsi
-        for i, value in enumerate(rightSet):
+        # leftSet by mal byt vacsi
+        for i, value in enumerate(leftSet):
             root = self.addNode(root, value, 0)
-            if i < len(leftSet):
-                root = self.addNode(root, leftSet[i], 0)
+            if i < len(rightSet):
+                root = self.addNode(root, rightSet[i], 0)
 
         return root
 
     # depth 0 -> 1 -> 2 -> ...; na kazdej parnej sa rozhoduje podla value[0], neparnej value[1]
+    # zle!?.. :(
     def addNode(self, node, value, depth):
 
         if not depth % 2:
-            if node.value[0] > value[0]:
+            if node.value[0] < value[0]:
                 if node.right:
                     node.right = self.addNode(node.right, value, depth+1)
                     return node
                 else:
                     node.right = Node(value)
                     return node
-            elif node.value[0] < value[0]:
+            elif node.value[0] > value[0]:
                 if node.left:
                     node.left = self.addNode(node.left, value, depth+1)
                     return node
@@ -58,14 +59,14 @@ class KDTree:
                     node.left = Node(value)
                     return node
             else:
-                if node.value[1] < value[1]:
+                if node.value[1] > value[1]:
                     if node.left:
                         node.left = self.addNode(node.left, value, depth + 1)
                         return node
                     else:
                         node.left = Node(value)
                         return node
-                elif node.value[1] >= value[1]:
+                elif node.value[1] <= value[1]:
                     if node.right:
                         node.right = self.addNode(node.right, value, depth + 1)
                         return node
@@ -73,36 +74,92 @@ class KDTree:
                         node.right = Node(value)
                         return node
         else:
-            if node.value[0] > value[0]:
-                if node.left:
-                    node.left = self.addNode(node.left, value, depth + 1)
-                    return node
-                else:
-                    node.left = Node(value)
-                    return node
-            elif node.value[0] < value[0]:
+            if node.value[1] < value[1]:
                 if node.right:
                     node.right = self.addNode(node.right, value, depth + 1)
                     return node
                 else:
                     node.right = Node(value)
                     return node
+            elif node.value[1] > value[1]:
+                if node.left:
+                    node.left = self.addNode(node.left, value, depth + 1)
+                    return node
+                else:
+                    node.left = Node(value)
+                    return node
             else:
-                if node.value[1] < value[1]:
-                    if node.right:
-                        node.right = self.addNode(node.right, value, depth + 1)
-                        return node
-                    else:
-                        node.right = Node(value)
-                        return node
-                elif node.value[1] >= value[1]:
+                if node.value[0] > value[0]:
                     if node.left:
                         node.left = self.addNode(node.left, value, depth + 1)
                         return node
                     else:
                         node.left = Node(value)
+                        return node
+                elif node.value[0] <= value[0]:
+                    if node.right:
+                        node.right = self.addNode(node.right, value, depth + 1)
+                        return node
+                    else:
+                        node.right = Node(value)
         return node
 
+    def findNearestKD(self, point, node, depth, shortest):
+        # Go to the leaf, in every node measure dist
+        # Ak sme v liste, vypocitaj kolmu vzdialenost k rodicovi
+        # Ak nie je blizsia ako najblizsia namerana, idem hore
+        # Ak je blizsia, vnorim sa
+
+        dist = getDistance(point, node.value)
+        if dist < shortest:
+            shortest = dist
+
+        if node.right != None and node.left != None:
+            if not depth % 2:
+                if point[0] > node.value[0]:
+                    shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+                elif point[0] < node.value[0]:
+                    shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+                else:
+                    if point[1] > node.value[1]:
+                        shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+                    elif point[1] <= node.value[1]:
+                        shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+            elif depth % 2:
+                if point[1] > node.value[1]:
+                    shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+                elif point[1] < node.value[1]:
+                    shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+                else:
+                    if point[0] > node.value[0]:
+                        shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+                    elif point[0] <= node.value[0]:
+                        shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+
+        elif node.right == None and node.left != None:
+            shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+
+        elif node.right != None and node.left == None:
+            shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+
+        else:
+            return shortest
+
+        # cesta spat
+        if not depth % 2:
+            if abs(point[0] - node.value[0]) < shortest:
+                if node.right != None and point[0] - node.value[0] < 0 :
+                    shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+                elif node.left != None and point[0] - node.value[0] >= 0 :
+                    shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+        else:
+            if abs(point[1] - node.value[1]) < shortest:
+                if node.right != None and point[1] - node.value[1] < 0:
+                    shortest = self.findNearestKD(point, node.right, depth + 1, shortest)
+                elif node.left != None and point[1] - node.value[1] >= 0:
+                    shortest = self.findNearestKD(point, node.left, depth + 1, shortest)
+
+        return shortest
 
 
 def init():
@@ -191,14 +248,17 @@ if __name__ == "__main__":
     kdtree = KDTree(trainingSet)
 
 
-    for i in range(500):
+    for i in range(1):
         p = generatePoint(i % 4)
         nearest = demoFindNearest(trainingSet,p , k)
+        print(nearest)
         p = np.append(p, checkMajority(nearest))
         # trainingSet = np.append(trainingSet, [p], axis=0)
+        a = kdtree.findNearestKD(p, kdtree.tree, 0, 15000)
+        print(f"__{a}")
         kdtree.addNode(kdtree.tree, p, 0)
 
-print(trainingSet)
+# print(trainingSet)
 
 # notes:
 # 100_000 /
